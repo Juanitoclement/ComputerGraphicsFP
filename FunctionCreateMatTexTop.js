91b9e2084acf4069aaf2faea14130f9f32a4fac2
Juanito Clement Tanjung <juanitoclementtanjung@192.168.1.178>
2018-06-05 20:53:25 +0700

    jetpackcamera

--- FunctionCreateMatTexTop.js
+++ FunctionCreateMatTexTop.js
@@ -1,7 +1,7 @@
 function createMatTexTop(scene){
   var materialTex = new BABYLON.StandardMaterial("materialTex", scene);
   materialTex.diffuseTexture = new BABYLON.Texture("sky.jpg", scene);
-  materialTex.specularTexture = new BABYLON.Texture("sky.png", scene);
+  materialTex.specularTexture = new BABYLON.Texture("sky.jpg", scene);
   materialTex.specularPower = 25
   //mat.emissiveColor = new BABYLON.Color3(0.2, 0.2, 0.2);
   materialTex.ambientColor = new BABYLON.Color3(0.23,0.98,0.53);
