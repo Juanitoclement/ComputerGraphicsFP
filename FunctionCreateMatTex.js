202bb1369212563e22c58f43c39a1790f1f2fced
Juanito Clement Tanjung <juanitoclementtanjung@192.168.88.164>
2018-06-06 18:13:44 +0700

    hahahahhahha

--- FunctionCreateMatTex.js
+++ FunctionCreateMatTex.js
@@ -1,7 +1,7 @@
 function createMatTex(scene){
   var materialTex = new BABYLON.StandardMaterial("materialTex", scene);
   materialTex.diffuseTexture = new BABYLON.Texture("ground.jpg", scene);
-  materialTex.specularTexture = new BABYLON.Texture("mario.jpg", scene);
+  materialTex.specularTexture = new BABYLON.Texture("house.jpg", scene);
   materialTex.specularPower = 25
   //mat.emissiveColor = new BABYLON.Color3(0.2, 0.2, 0.2);
   materialTex.ambientColor = new BABYLON.Color3(0.23,0.98,0.53);
--- FunctionCreateMatTexSide.js
+++ FunctionCreateMatTexSide.js
@@ -1,7 +1,7 @@
 function createMatTexSide(scene){
   var materialTex = new BABYLON.StandardMaterial("materialTex", scene);
   materialTex.diffuseTexture = new BABYLON.Texture("side.jpg", scene);
-  materialTex.specularTexture = new BABYLON.Texture("mario.jpg", scene);
+  materialTex.specularTexture = new BABYLON.Texture("side.jpg", scene);
   materialTex.specularPower = 25
   //mat.emissiveColor = new BABYLON.Color3(0.2, 0.2, 0.2);
   materialTex.ambientColor = new BABYLON.Color3(0.23,0.98,0.53);
